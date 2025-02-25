dedd03630676398551abe40c2d13cfad6945bc6f
Mourt <Mourt2@proton.me>
2025-02-25 06:04:19 -0600

    Add examples
    Added examples for both functions

---
+++ examples/extendedMillis/extendedMillis.ino
@@ -0,0 +1,21 @@
+/*
+Simply use extendedMillis as you would millis, but the returned type is unsigned long long. Other than that, it will work the same, but not roll over for much, much longer.
+Enjoy
+*/
+
+#include <ExtendedTime.h>
+int LED = LED_BUILTIN;
+unsigned long long now;
+void setup() {
+  pinMode(LED, OUTPUT);
+  now = extendedMicros();
+}
+
+void loop() {
+  if (extendedMicros() - now > 1000000)
+  {
+    digitalWrite(LED, !digitalRead(LED));
+    now = extendedMicros();
+  }
+
+}
